18ceb3d8af3427f41bcec2d5e3116a0fb20178be
Aman Thakur <amant8425@gmail.com>
2024-06-22 18:03:19 +0530

    updated

---
+++ app.js
@@ -0,0 +1,40 @@
+const inp = document.querySelector('#input')
+
+function createE(){
+    const todoData = document.querySelector("#tododata")
+const newDiv = document.createElement("div")
+const checkbox = document.createElement("input")
+checkbox.type = 'checkbox'
+checkbox.value ='false'
+checkbox.className   = 'checkbox' 
+const newDivText = document.createTextNode(inp.value)
+newDiv.appendChild(newDivText)
+const currentDiv =document.getElementById("div1")
+todoData.insertBefore(newDiv,currentDiv)
+newDiv.insertBefore(checkbox,newDivText )
+
+checkbox.addEventListener('change',function(){
+    if(checkbox.checked){
+         newDiv.remove()
+         const completedData = document.querySelector("#completeddata")
+        
+        completedData.insertBefore(newDiv,currentDiv)
+newDiv.insertBefore(checkbox,newDivText )
+
+       
+    }
+    else{
+        console.log(false);
+    }
+})
+}
+// console.log(checkbox.value);
+function addElement(){
+    const text = inp.value.trim()
+    if(text !== ""){
+      
+        createE()
+       
+    }
+}
+
